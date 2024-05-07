add7d2a2cc5d7773c553519b65a36d291a2c2ef2
BlazeIsGoodAtProgramming <154691571+BlazeIsGoodAtProgramming@users.noreply.github.com>
2024-05-07 20:40:28 +0330

    create

--- root/urls.py
+++ root/urls.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from .views import * 
 
+app_name ='root'
 
 urlpatterns = [
-    path('',func_home),
-    path('detail/',func_detail),
+    path('',func_home , name = 'home'),
+    path('detail/',func_detail , name = 'detail' ),
 ]
